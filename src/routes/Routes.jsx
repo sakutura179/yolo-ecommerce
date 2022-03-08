ee74ff55cda59671ddc30dfb4e7723664e956530
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-08 16:09:36 +0700

    Set up routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Catalog from '../pages/Catalog'
+import Cart from '../pages/Cart'
+import Product from '../pages/Product'
 
 const Routes = () => {
     return (
-        <div>Routes</div>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/catalog" component={Catalog} />
+            <Route path="/catalog/:slug" component={Product} />
+            <Route path="/cart" component={Cart} />
+        </Switch>
     )
 }
 
