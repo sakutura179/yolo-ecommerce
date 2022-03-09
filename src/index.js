6c309f53fa388ab210b02e5f381233cb6006c65d
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-09 15:38:46 +0700

    Upgrade from boxicons 2.0.7 to 2.1.2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './assets/boxicons-2.1.2/css/boxicons.min.css';
 import './sass/index.scss';
 import Layout from './components/Layout';
 
