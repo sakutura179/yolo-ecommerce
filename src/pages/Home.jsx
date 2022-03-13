ae67f5377eff3030c75661c6234e96cc5b5032ac
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-13 14:28:31 +0700

    Make slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
 import Helmet from '../components/Helmet'
+import Slider from '../components/Slider'
+import sliderData from '../assets/fake-data/slider'
 
 const Home = () => {
     return (
-        <Helmet title='Trang chủ'>Home</Helmet>
+        <Helmet title='Trang chủ'>
+            {/* Slider */}
+            <Slider
+                data={sliderData}
+                control={true}
+                auto={false}
+            />
+            {/* End slider */}
+        </Helmet>
     )
 }
 
