41dc40bb1ae5d12f2a519a67cb310f0309f827de
sakutura179 <khanhnguyen3916@gmail.com>
2022-03-13 21:18:21 +0700

    Make section and policy card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 import Helmet from '../components/Helmet'
 import Slider from '../components/Slider'
+import Grid from '../components/Grid'
+import Section, { SectionBody, SectionTitle } from '../components/Section'
+import PolicyCart from '../components/PolicyCart'
+
 import sliderData from '../assets/fake-data/slider'
+import policy from '../assets/fake-data/policy'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
     return (
@@ -14,6 +21,30 @@ const Home = () => {
                 auto={false}
             />
             {/* End slider */}
+            {/* Policy section */}
+            <Section>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+                    >
+                        {
+                            policy.map((item, index) => (
+                                <Link to="/policy" key={index}>
+                                    <PolicyCart
+                                        name={item.name}
+                                        description={item.description}
+                                        icon={item.icon}
+                                    />
+                                </Link>
+                            ))
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+            {/* End Policy section */}
         </Helmet>
     )
 }
