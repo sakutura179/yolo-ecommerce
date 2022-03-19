54da24353a428b4a53de49894a77e0de93558227
khanhs <khanhnguyen3916@gmail.com>
2022-03-19 16:26:44 +0700

    Make catalog filter

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types'
 const CheckBox = props => {
     const inputRef = React.useRef(null);
 
+    // onChange o day la ten ham
     const onChange = () => {
         if (props.onChange) {
             // su dung de truyen len catalog xu ly tiep
+            // onChange o day la thuoc tinh cua component
             props.onChange(inputRef.current)
         }
     };
@@ -24,7 +26,8 @@ const CheckBox = props => {
 
 CheckBox.propTypes = {
     label: PropTypes.string.isRequired,
-    checked: PropTypes.bool
+    checked: PropTypes.bool,
+    onChange: PropTypes.func
 }
 
 export default CheckBox
