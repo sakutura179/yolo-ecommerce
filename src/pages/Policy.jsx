b2dfa134847c077b739cc8772e5a7771d291a2cb
khanhs <khanhnguyen3916@gmail.com>
2022-03-22 15:36:57 +0700

    Make policy page

--- src/pages/Policy.jsx
+++ src/pages/Policy.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 
+import Helmet from '../components/Helmet'
+
+const policyContent = [
+    {
+        'title': 'Chính sách đổi trả',
+        'content': '<p>- Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p><p>- Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p>'
+    },
+    {
+        'title': 'Chính sách hoàn tiền',
+        'content': '<p>- Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p><p>- Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p>'
+    },
+    {
+        'title': 'Chính sách bảo hành',
+        'content': '<p>- Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p><p>- Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p>'
+    },
+    {
+        'title': 'Khách hàng VIP',
+        'content': '<p>- Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p><p>- Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.</p>'
+    }
+]
+
 const Policy = () => {
     return (
-        <div>Policy</div>
+        <Helmet title='Chính sách'>
+            <div className='policy'>
+                {
+                    policyContent.map((item, index) => (
+                        <div key={index} className='policy__item'>
+                            <div className='policy__item__title'>
+                                {item.title}
+                            </div>
+                            <div dangerouslySetInnerHTML={{ __html: item.content }} className='policy__item__content'>
+
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </Helmet>
     )
 }
 
