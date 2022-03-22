87a4a00409446940017893ac6bf2e3203a2b98e6
khanhs <khanhnguyen3916@gmail.com>
2022-03-22 14:58:33 +0700

    Make about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import Helmet from '../components/Helmet'
+
 const About = () => {
     return (
-        <div>About</div>
+        <Helmet title='Giới thiệu'>
+            <div className='about'>
+                <div className='about__title'>
+                    về chúng tôi
+                </div>
+                <div className='about__content'>
+                    <p className='about__content__item'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.
+                    </p>
+                    <p className='about__content__item'>
+                        Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus.
+                    </p>
+                    <p className='about__content__item'>
+                        Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl nunc eget purus. Sed euismod, urna eu tincidunt consectetur, nisi nunc malesuada urna, eget tincidunt nisl n
+                    </p>
+                    <p className='about__content__slogan'>
+                        Lorem ipsum - YOLO
+                    </p>
+                </div>
+            </div>
+        </Helmet>
     )
 }
 
